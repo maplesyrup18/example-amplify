efa7f68b116309c00a1951efa7061a62a07dc96b
Maplesyrup18 <aceasa08@hotmail.com>
2026-02-13 13:57:06 +0000

    auth module

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,21 @@
 import { defineBackend } from '@aws-amplify/backend';
+import * as cognito from 'aws-cdk-lib/aws-cognito';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
+});
+
+/* Define Admin group auth stack */
+const userPoolId = backend.auth.resources.userPool.userPoolId;
+const authGroupStack = backend.createStack('AuthGroupStack');
+const adminGroup = new cognito.CfnUserPoolGroup(authGroupStack, 'AdminGroup', {
+  userPoolId,
+  groupName: 'Admin',
+  description: 'Admin group',
 });
