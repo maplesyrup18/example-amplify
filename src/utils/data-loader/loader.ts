49b445f56a69a0c4bb4fd232a62a6ed5ef0fe110
Maplesyrup18 <aceasa08@hotmail.com>
2026-02-13 14:29:28 +0000

    form components added

--- src/utils/data-loader/loader.ts
+++ src/utils/data-loader/loader.ts
@@ -1,7 +1,7 @@
 import type { Schema } from "../../../amplify/data/resource";
 import { generateClient } from 'aws-amplify/api';
 
-const SOURCE_BUCKET_URL = '<insert CloudFront URL here>';
+const SOURCE_BUCKET_URL = 'http://dgdxkjr29p1f7.cloudfront.net';
 
 import categories from "./categories.json";
 import products from "./products.json";
